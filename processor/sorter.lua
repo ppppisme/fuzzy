0d13fb8896c6aa13b03e262ddfb478f06ee7b024
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-11-01 21:01:03 +0600

    Add order parameter for sorter processor

--- processor/sorter.lua
+++ processor/sorter.lua
@@ -1,17 +1,25 @@
+local utils = require("fuzzy.utils")
+
 local processor = {}
 
 local sort_by
-local utils
+local order
 
 function processor.init(options)
   sort_by = options.sort_by
-  utils = require("fuzzy.utils")
+  order = options.order or "DESC"
 end
 
 function processor.process(list, _)
-  table.sort(list, function(a, b)
-    return utils.extract_value(a, sort_by) > utils.extract_value(b, sort_by)
-  end)
+  if order == "DESC" then
+    table.sort(list, function(a, b)
+      return utils.extract_value(a, sort_by) > utils.extract_value(b, sort_by)
+    end)
+  else
+    table.sort(list, function(a, b)
+      return utils.extract_value(a, sort_by) < utils.extract_value(b, sort_by)
+    end)
+  end
 
   return list
 end
