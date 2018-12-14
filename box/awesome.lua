529716366612e81a6b9d6909339b74812e2bbb72
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-12-14 18:02:30 +0600

    Reset list items when awesome box is shown

--- box/awesome.lua
+++ box/awesome.lua
@@ -123,6 +123,8 @@ function box.show(list, process_callback, exe_callback)
   widget.x = screen_geometry.x + (screen_geometry.width - widget.width) / 2
   widget.y = screen_geometry.y + (screen_geometry.height - widget.height) / 2
 
+  update_list {}
+
   local processed_list
 
   local process_wrapper = function(list, input)
