834ec115f37b9b4f27cc5804525e9460eb47f8b0
Vladislav Gorenkin <vladgorenkin@yandex.ru>
2019-09-26 14:37:41 +0600

    Add source for manipulating focused client properties

--- source.lua
+++ source.lua
@@ -1,5 +1,6 @@
 local os
 local io
+local awful
 
 local sources = {}
 
@@ -52,6 +53,69 @@ function sources.client()
   end
 
   return output
+end
+
+function sources.client_options()
+  local focused_client = client.focus -- luacheck: globals client
+
+  return {
+    {
+      title = focused_client.sticky and "Unset sticky" or "Set sticky",
+      description = "The client sticky, i.e. available on all tags",
+      value = function () focused_client.sticky = not focused_client.sticky end,
+    },
+    {
+      title = focused_client.floating and "Unset floating" or "Set floating",
+      description = "The client floating state. If the client is part of the tiled layout or free floating",
+      value = function () focused_client.floating = not focused_client.floating end,
+    },
+    {
+      title = focused_client.maximized and "Unset maximized" or "Set maximized",
+      description = "The client is maximized (horizontally and vertically) or not",
+      value = function () focused_client.maximized = not focused_client.maximized end,
+    },
+    {
+      title = focused_client.ontop and "Unset ontop" or "Set ontop",
+      description = "The client is on top of every other windows",
+      value = function () focused_client.ontop = not focused_client.ontop end,
+    },
+    {
+      title = focused_client.fullscreen and "Unset fullscreen" or "Set fullscreen",
+      description = "The client is fullscreen or not",
+      value = function () focused_client.fullscreen = not focused_client.fullscreen end,
+    },
+    {
+      title = focused_client.picture_in_picture and "Unset Picture-in-picture mod" or "Set Picture-in-picture mod",
+      description = "Move client to the right bottom, make it smaller and sticky ",
+      value = function ()
+        if not focused_client.picture_in_picture then
+          focused_client.ontop = true
+          focused_client.sticky = true
+          focused_client.floating = true
+          focused_client.focusable = false
+
+          if not awful then
+            awful = require("awful")
+          end
+          local screen_geometry = awful.screen.focused().geometry
+
+          focused_client.width = screen_geometry.width * 0.3
+          focused_client.height = focused_client.width * 0.5625
+          focused_client.x = screen_geometry.width - 16 - focused_client.width
+          focused_client.y = screen_geometry.height - 16 - focused_client.height
+
+          focused_client.picture_in_picture = true
+        else
+          focused_client.ontop = false
+          focused_client.sticky = false
+          focused_client.floating = false
+          focused_client.focusable = true
+
+          focused_client.picture_in_picture = false
+        end
+      end,
+    },
+  }
 end
 
 return sources
